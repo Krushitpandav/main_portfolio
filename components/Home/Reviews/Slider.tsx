482bc353556c1dd681ab3055c0015f069dfd5f0c
Jenesh <jeneshrambo18@gmail.com>
2025-05-03 20:27:42 +0530

    first commit

--- components/Home/Reviews/Slider.tsx
+++ components/Home/Reviews/Slider.tsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import ReviewCard from './ReviewCard';
+// import ReviewCard from './ReviewCard';
 
 const responsive = {
     desktop: {
